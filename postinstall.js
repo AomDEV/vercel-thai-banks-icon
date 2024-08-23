6520476051f9197c2920de2e0b78816f8e4656b1
Aom Siriwat <siriwat576@gmail.com>
2024-08-24 00:12:43 +0700

    chore: Add comment for `postinstall.js` file

--- postinstall.js
+++ postinstall.js
@@ -5,14 +5,18 @@ const getModuleIconFolder = () => path.join(__dirname, 'node_modules', 'css-fina
 const getVercelIconFolder = () => path.join(__dirname, '.vercel', 'output', 'static', 'icons')
 const getStaticIconFolder = () => path.join(__dirname, 'public', 'icons');
 async function main() {
+    /* For Development */
     if (!fs.existsSync(getStaticIconFolder())) fs.mkdirSync(getStaticIconFolder());
+    /* For Vercel */
     if (!fs.existsSync(getVercelIconFolder())) fs.mkdirSync(getVercelIconFolder());
     const files = fs.readdirSync(getModuleIconFolder());
     for (const file of files) {
+        /* For Development */
         fs.copyFileSync(
             path.join(getModuleIconFolder(), file),
             path.join(getStaticIconFolder(), path.basename(file))
         );
+        /* For Vercel */
         fs.copyFileSync(
             path.join(getModuleIconFolder(), file),
             path.join(getVercelIconFolder(), path.basename(file))
