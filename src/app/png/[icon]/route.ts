aecaa25a7fbd66004cb4a2a9f22cf8d5da2e2d4f
Aom Siriwat <siriwat576@gmail.com>
2024-08-24 00:24:36 +0700

    fix: Fix base path for vercel deployment

--- src/app/png/[icon]/route.ts
+++ src/app/png/[icon]/route.ts
@@ -1,4 +1,5 @@
 "use server";
+import { getBasePath } from "@/app/utils";
 import { IconHelper } from "css-finances"
 import { existsSync } from "fs";
 import { readFile } from "fs/promises";
@@ -19,12 +20,12 @@ export async function GET(
   const width = Number(req.nextUrl.searchParams.get("width") || 256)
   const height = Number(req.nextUrl.searchParams.get("height") || 256)
 
-  const hasNextFolder = existsSync(join(serverRuntimeConfig.PROJECT_ROOT, '_next'));
-  const hasIconFolder = existsSync(join(serverRuntimeConfig.PROJECT_ROOT, 'icons'));
+  const hasNextFolder = existsSync(join(getBasePath(), '_next'));
+  const hasIconFolder = existsSync(join(getBasePath(), 'icons'));
   const isVercelDeployed = hasNextFolder && hasIconFolder;
   const svgPath = await readFile(
     join(...[
-      serverRuntimeConfig.PROJECT_ROOT,
+      isVercelDeployed ? getBasePath() : serverRuntimeConfig.PROJECT_ROOT,
       isVercelDeployed && 'public',
       'icons',
       `${icon}.svg`
