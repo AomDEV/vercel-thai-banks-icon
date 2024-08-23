aea18087797c3238037008b5ff8bc084b93469d6
Aom Siriwat <siriwat576@gmail.com>
2024-08-24 00:32:08 +0700

    fix: Get static icon files from URL

--- src/app/png/[icon]/route.ts
+++ src/app/png/[icon]/route.ts
@@ -1,11 +1,6 @@
 "use server";
-import { getBasePath } from "@/app/utils";
 import { IconHelper } from "css-finances"
-import { existsSync } from "fs";
-import { readFile } from "fs/promises";
-import getConfig from "next/config";
 import { NextRequest, NextResponse } from "next/server";
-import { join } from "path";
 
 type RouteProps = {
   readonly params: {
@@ -16,26 +11,17 @@ export async function GET(
   req: NextRequest,
   { params: { icon } }: RouteProps
 ) {
-  const { serverRuntimeConfig } = getConfig()
   const width = Number(req.nextUrl.searchParams.get("width") || 256)
   const height = Number(req.nextUrl.searchParams.get("height") || 256)
 
-  const hasNextFolder = existsSync(join(getBasePath(), '_next'));
-  const hasIconFolder = existsSync(join(getBasePath(), 'icons'));
-  const isVercelDeployed = hasNextFolder && hasIconFolder;
-  const svgPath = await readFile(
-    join(...[
-      isVercelDeployed ? getBasePath() : serverRuntimeConfig.PROJECT_ROOT,
-      isVercelDeployed && 'public',
-      'icons',
-      `${icon}.svg`
-    ].filter(Boolean))
-  );
+  const staticUrl = new URL(`/icons/${icon}.svg`, req.url);
+  console.log(staticUrl.toString());
+  const svgPath = await fetch(staticUrl.toString()).then((r) => r.arrayBuffer())
   const imageBuffer = await IconHelper.getBuffer(icon, {
     width,
     height,
     format: "png",
-    buffer: svgPath,
+    buffer: Buffer.from(svgPath),
   });
   return new NextResponse(imageBuffer)
 }
--- src/app/utils.ts
+++
@@ -1,1 +0,0 @@
-export const getBasePath = () => __dirname;
