f54b628ff6c9e3e042fbc48fb1a5ccb6019b65af
Aom Siriwat <siriwat576@gmail.com>
2024-08-23 23:49:47 +0700

    chore: Read svg files from public folder

--- src/app/png/[icon]/route.ts
+++ src/app/png/[icon]/route.ts
@@ -1,6 +1,9 @@
 "use server";
 import { IconHelper } from "css-finances"
+import { readFile } from "fs/promises";
+import getConfig from "next/config";
 import { NextRequest, NextResponse } from "next/server";
+import { join } from "path";
 
 type RouteProps = {
   readonly params: {
@@ -11,13 +14,16 @@ export async function GET(
   req: NextRequest,
   { params: { icon } }: RouteProps
 ) {
+  const { serverRuntimeConfig } = getConfig()
   const width = Number(req.nextUrl.searchParams.get("width") || 256)
   const height = Number(req.nextUrl.searchParams.get("height") || 256)
 
+  const svgPath = await readFile(join(serverRuntimeConfig.PROJECT_ROOT, 'public', 'icons', `${icon}.svg`));
   const imageBuffer = await IconHelper.getBuffer(icon, {
     width,
     height,
-    format: "png"
+    format: "png",
+    buffer: svgPath,
   });
   return new NextResponse(imageBuffer)
 }
