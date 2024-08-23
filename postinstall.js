7b02c58238e1ed6fee81ecc81e344ef8e0ebc34c
Aom Siriwat <siriwat576@gmail.com>
2024-08-23 23:49:19 +0700

    feat: Add postinstall hook script

---
+++ postinstall.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+const getModuleIconFolder = () => path.join(__dirname, 'node_modules', 'css-finances', 'svg');
+const getStaticIconFolder = () => path.join(__dirname, 'public', 'icons');
+async function main() {
+    if (!fs.existsSync(getStaticIconFolder())) fs.mkdirSync(getStaticIconFolder());
+    const files = fs.readdirSync(getModuleIconFolder());
+    for (const file of files) {
+        fs.copyFileSync(
+            path.join(getModuleIconFolder(), file),
+            path.join(getStaticIconFolder(), path.basename(file))
+        );
+    }
+}
+main().catch((e) => {
+    console.error(e);
+    process.exit(1);
+})
