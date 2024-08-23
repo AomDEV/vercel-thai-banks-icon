ed4a115715ff44998514df146679f7bd98155134
Aom Siriwat <siriwat576@gmail.com>
2024-08-23 21:29:39 +0700

    chore: Use function from `css-finances` library

--- src/app/png/[icon]/route.ts
+++ src/app/png/[icon]/route.ts
@@ -1,11 +1,6 @@
 "use server";
-import IconJson from "css-finances/banks.json"
-import sharp from "sharp";
-import { IconSchema } from "@/types";
+import { IconHelper } from "css-finances"
 import { NextRequest, NextResponse } from "next/server";
-import { readFile } from "fs/promises";
-import { join } from "path";
-const node_modules = require('node_modules-path');
 
 type RouteProps = {
   readonly params: {
@@ -18,28 +13,11 @@ export async function GET(
 ) {
   const width = Number(req.nextUrl.searchParams.get("width") || 256)
   const height = Number(req.nextUrl.searchParams.get("height") || 256)
-  const iconSchema = IconJson.th as IconSchema;
 
-  const svgFilePath = join(
-    node_modules(),
-    `css-finances/svg/${icon}.svg`
-  );
-  const svgBuffer = await readFile(svgFilePath)
-  const iconBuffer = await sharp(svgBuffer).resize(Number(width / 2)).toBuffer();
-
-  const canvas = sharp({
-    create: {
-      width,
-      height,
-      channels: 4,
-      background: iconSchema[icon].color
-    }
+  const imageBuffer = await IconHelper.getBuffer(icon, {
+    width,
+    height,
+    format: "png"
   });
-  canvas.composite([{
-    input: iconBuffer,
-    blend: "over",
-  }])
-
-  const image = await canvas.png().toBuffer();
-  return new NextResponse(image)
+  return new NextResponse(imageBuffer)
 }
