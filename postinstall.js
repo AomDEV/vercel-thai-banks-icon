386b9dd01f192069848fd24548aadf7afa625970
Aom Siriwat <siriwat576@gmail.com>
2024-08-24 00:10:04 +0700

    chore: Move all static icons to `.vercel` folder

--- postinstall.js
+++ postinstall.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const getModuleIconFolder = () => path.join(__dirname, 'node_modules', 'css-finances', 'svg');
+const getVercelIconFolder = () => path.join(__dirname, '.vercel', 'output', 'static', 'icons')
 const getStaticIconFolder = () => path.join(__dirname, 'public', 'icons');
 async function main() {
     if (!fs.existsSync(getStaticIconFolder())) fs.mkdirSync(getStaticIconFolder());
@@ -10,6 +11,10 @@ async function main() {
         fs.copyFileSync(
             path.join(getModuleIconFolder(), file),
             path.join(getStaticIconFolder(), path.basename(file))
+        );
+        fs.copyFileSync(
+            path.join(getModuleIconFolder(), file),
+            path.join(getVercelIconFolder(), path.basename(file))
         );
     }
 }
