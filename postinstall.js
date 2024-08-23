2a3d2af72cc5d295d37be2508a3a20fe482f2eab
Aom Siriwat <siriwat576@gmail.com>
2024-08-24 00:14:10 +0700

    chore: Remove vercel file preparation

--- postinstall.js
+++ postinstall.js
@@ -2,24 +2,16 @@ const fs = require('fs');
 const path = require('path');
 
 const getModuleIconFolder = () => path.join(__dirname, 'node_modules', 'css-finances', 'svg');
-const getVercelIconFolder = () => path.join(__dirname, '.vercel', 'output', 'static', 'icons')
 const getStaticIconFolder = () => path.join(__dirname, 'public', 'icons');
 async function main() {
     /* For Development */
     if (!fs.existsSync(getStaticIconFolder())) fs.mkdirSync(getStaticIconFolder());
-    /* For Vercel */
-    if (!fs.existsSync(getVercelIconFolder())) fs.mkdirSync(getVercelIconFolder());
     const files = fs.readdirSync(getModuleIconFolder());
     for (const file of files) {
         /* For Development */
         fs.copyFileSync(
             path.join(getModuleIconFolder(), file),
             path.join(getStaticIconFolder(), path.basename(file))
-        );
-        /* For Vercel */
-        fs.copyFileSync(
-            path.join(getModuleIconFolder(), file),
-            path.join(getVercelIconFolder(), path.basename(file))
         );
     }
 }
